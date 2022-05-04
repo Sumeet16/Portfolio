e5ae1c785ce630cd149a5235bc12fc41076be523
Sumeet Yadav <sumeetyadav@192.168.1.9>
2022-05-04 13:42:07 +0530

    fix some other bug

--- src/Components/Aboutme.js
+++ src/Components/Aboutme.js
@@ -33,7 +33,7 @@ const Aboutme = () => {
                     </div>
                 </div>
 
-                <a href="http://www.google.com" target="_blank">
+                <a href="https://drive.google.com/file/d/1LIrTKnSG8nfbDK8EtUkc9cCQGO3BLMs4/view?usp=sharing" target="_blank">
                     <div className="about__contact__btn">
                         <p className="contact__btn__txt">Download CV</p>
                         <UilDownloadAlt />
